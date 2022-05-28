b5c5f18ca7576cdf7bb365e9fcd291369af6a089
Caesar0910 <cooker98910@gmail.com>
2022-05-28 21:12:54 +0900

    contract

--- contracts/token/onft/extension/UniversalONFT721.sol
+++ contracts/token/onft/extension/UniversalONFT721.sol
@@ -8,6 +8,7 @@ import "../ONFT721.sol";
 contract UniversalONFT721 is ONFT721 {
     uint public nextMintId;
     uint public maxMintId;
+    uint public mintPrice;
 
     /// @notice Constructor for the UniversalONFT
     /// @param _name the name of the token
@@ -18,15 +19,30 @@ contract UniversalONFT721 is ONFT721 {
     constructor(string memory _name, string memory _symbol, address _layerZeroEndpoint, uint _startMintId, uint _endMintId) ONFT721(_name, _symbol, _layerZeroEndpoint) {
         nextMintId = _startMintId;
         maxMintId = _endMintId;
+        mintPrice = 1;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return 'https://gateway.pinata.cloud/ipfs/QmcV6GKttyjpqWoMhbC7Jn5XYhx6Gwj8vFejzwv1pffwu5/';
+    }
+
+    function setMinPrice(uint _price) external onlyOwner {
+        mintPrice = _price;
     }
 
     /// @notice Mint your ONFT
     function mint() external payable {
         require(nextMintId <= maxMintId, "UniversalONFT721: max mint limit reached");
+        require(msg.value >= mintPrice, "Not enough tokens sent");
 
         uint newId = nextMintId;
         nextMintId++;
 
         _safeMint(msg.sender, newId);
+    }
+
+    function withdraw() public onlyOwner {
+        (bool success, ) = payable(owner()).call{value: address(this).balance}("");
+        require(success);
     }
 }
