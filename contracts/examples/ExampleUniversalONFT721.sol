1a672b4ad521f7f495c58df62a651be21bef309c
Caesar0910 <cooker98910@gmail.com>
2022-05-29 22:31:49 +0900

    omnistcik

--- contracts/examples/ExampleUniversalONFT721.sol
+++ contracts/examples/ExampleUniversalONFT721.sol
@@ -44,11 +44,12 @@ pragma solidity 0.8.4;
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 import "../token/onft/extension/UniversalONFT721.sol";
+import "../token/onft/extension/OmniSticks.sol";
 
 /// @title A LayerZero UniversalONFT example
 /// @notice You can use this to mint ONFT and send nftIds across chain.
 ///  Each contract deployed to a chain should carefully set a `_startMintIndex` and a `_maxMint`
 ///  value to set a range of allowed mintable nftIds (so that no two chains can mint the same id!)
-contract ExampleUniversalONFT721 is UniversalONFT721 {
+contract ExampleUniversalONFT721 is OmniSticks {
     constructor(address _layerZeroEndpoint, uint _startMintId, uint _endMintId) UniversalONFT721("ExampleUniversalONFT721", "ONFT721", _layerZeroEndpoint, _startMintId, _endMintId) {}
 }
