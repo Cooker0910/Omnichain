2cbc6c0e9fdb1117840054f3a8d5d7581220c376
Caesar0910 <cooker98910@gmail.com>
2022-05-29 22:19:18 +0900

    update

---
+++ scripts/deploy.js
@@ -0,0 +1,39 @@
+const { ethers, network } = require('hardhat');
+const hre = require('hardhat');
+const LZ_ENDPOINTS = require("../constants/layerzeroEndpoints.json");
+
+async function main () {
+  const [deployer] = await ethers.getSigners();
+  const networkName = hre.network.name;
+
+  console.log("Deploying contracts with the account", deployer.address);
+
+  console.log("Account balance", (await deployer.getBalance()).toString());
+
+  const omniSticksFactory = await ethers.getContractFactory("OmniSticks");
+
+  let omniSticksContract;
+
+  switch(network) {
+    case 'rinkeby':
+      omniSticksContract = await omniSticksFactory.deploy("Omni Sticks Testnet", "STICKS", LZ_ENDPOINTS.rinkeby, 0, 749);
+      break
+    case 'mumbai':
+      omniSticksContract = await omniSticksContract.deploy("Omni Sticks Testnet", "STICKS", LZ_ENDPOINTS.mumbai, 750, 1499);
+      break
+    default:
+      console.log("that is not a valid network name");
+  }
+  console.log("Token address:", omniSticksContract.address);
+
+  await greeter.deployed();
+
+  console.log("Greeter deployed to:", greeter.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  })
